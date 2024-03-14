525a9baab90c0747e877e4269eeab2232614aa8e
Shivangi Mall <mallshivangi03@gmail.com>
2024-03-14 18:18:42 +0530

    message

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const urlRoute = require("./routes/url")
+const {connectToMongoDB}  = require('./connection')
+const URL = require("./models/url")
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(()=> console.log("MongoDb connected.."));
+
+
+app.use(express.json());
+app.use("/url",urlRoute);
+
+app.get("/:shortId",async (req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    },{$push:{
+        visitHistory:{
+            timeStamp: Date.now()
+        }
+    }})
+
+    res.redirect(entry.redirectURL);
+})
+app.listen(PORT, ()=> console.log(`Server Started at ${PORT}`)); 
